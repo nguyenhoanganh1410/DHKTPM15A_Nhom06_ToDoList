bbc97d917249f8e45e637d2cd948ae8f04212b44
hoanganh <hoanganh1410tb@gmail.com>
2022-04-27 13:47:50 +0700

    1 - finished register screen

---
+++ app/src/main/java/com/example/dhktpm15a_nhom06_todoapp/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.dhktpm15a_nhom06_todoapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dhktpm15a_nhom06_todoapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
